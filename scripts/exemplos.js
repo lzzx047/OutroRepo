5240e6cb4da870c9505d5024f3d5661c237ea26f
Luiz <luizmarquesfernando037@gmail.com>
2026-03-23 21:57:20 -0300

    revisão da prova

---
+++ scripts/exemplos.js
@@ -0,0 +1,19 @@
+//-------------------------- Exemplo 1 -----------------------------//
+// function
+function exercicio1() {
+    alert("OIE")
+}
+// getElement
+const batata = document.getElementById("exercicio1")
+// addEventlistener
+batata.addEventListener('click' , () => { exercicio1() })
+
+//-------------------------- Exemplo 2 -----------------------------//
+// function
+function exercicio2() {
+    alert("OIE")
+}
+// getElement
+const buttonexercicio2 = document.getElementById("exercicio2")
+// addEventlistener
+buttonexercicio2.addEventListener('click' , () => { exercicio2() })
